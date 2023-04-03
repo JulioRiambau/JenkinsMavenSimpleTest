0eb5848f122ac40e568573f2cedbfa551e86d1bf
Julio Riambau <julio.riambau@skyline.be>
2023-04-03 13:33:36 -0400

    modified message

--- single-module/src/test/java/com/example/TestGreeter.java
+++ single-module/src/test/java/com/example/TestGreeter.java
@@ -36,7 +36,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World!";
+    String someone = "World!!";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
